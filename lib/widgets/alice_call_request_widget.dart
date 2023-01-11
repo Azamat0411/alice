8604ec0f106904b3ce592727c4eb91fd4ab26ee7
Azamat <69895564+Azamat0411@users.noreply.github.com>
2023-01-11 16:36:59 +0500

    Update alice_call_request_widget.dart

--- lib/widgets/alice_call_request_widget.dart
+++ lib/widgets/alice_call_request_widget.dart
@@ -23,41 +23,6 @@ class _AliceCallRequestWidget
   @override
   Widget build(BuildContext context) {
     final List<Widget> rows = [];
-    // rows.add(getListRow("Started:", _call.request!.time.toString()));
-    // rows.add(getListRow("Bytes sent:", formatBytes(_call.request!.size)));
-    // rows.add(
-    //   getListRow("Content type:", getContentType(_call.request!.headers)!),
-    // );
-    //
-    // final dynamic body = _call.request!.body;
- /*   var bodyContent = "Body is empty";
-    // if (body != null) {
-    //   bodyContent = formatBody(body, getContentType(_call.request!.headers));
-    // }
-    rows.add(getListRow("Body:", bodyContent));
-    final formDataFields = _call.request!.formDataFields;
-    if (formDataFields?.isNotEmpty == true) {
-      rows.add(getListRow("Form data fields: ", ""));
-      formDataFields!.forEach(
-        (field) {
-          rows.add(getListRow("   • ${field.name}:", field.value));
-        },
-      );
-    }
-    final formDataFiles = _call.request!.formDataFiles;
-    if (formDataFiles?.isNotEmpty == true) {
-      rows.add(getListRow("Form data files: ", ""));
-      formDataFiles!.forEach(
-        (field) {
-          rows.add(
-            getListRow(
-              "   • ${field.fileName}:",
-              "${field.contentType} / ${field.length} B",
-            ),
-          );
-        },
-      );
-    }*/
 
     final headers = _result.requestOptions.headers;
     var headersContent = "Headers are empty";
@@ -72,6 +37,7 @@ class _AliceCallRequestWidget
     
     if (_result.requestOptions.data != null) {
       var data = _result.requestOptions.data;
+      rows.add(getListRow("Data: ", ''));
       if (data is String) {
         data = jsonDecode(data);
         rows.add(getListRow("   • data:", data));
