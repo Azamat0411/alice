c405a42a80c676ae8777f9670477ec2ee84d6c0a
Azamat <69895564+Azamat0411@users.noreply.github.com>
2023-11-08 12:36:04 +0500

    Update alice_call_details_screen.dart
    update

--- lib/alice_call_details_screen.dart
+++ lib/alice_call_details_screen.dart
@@ -1,20 +1,20 @@
-import 'package:flutter/material.dart' as alica_call_details_screen;
+import 'package:flutter/material.dart';
 import 'widgets/alice_call_error_widget.dart';
 import 'widgets/alice_call_overview_widget.dart';
 import 'widgets/alice_call_request_widget.dart';
 import 'widgets/alice_call_response_widget.dart';
 
-class AliceCallDetailsScreen extends alica_call_details_screen.StatefulWidget {
+class AliceCallDetailsScreen extends StatefulWidget {
   final dynamic result;
 
-  const AliceCallDetailsScreen({alica_call_details_screen.Key? key, required this.result}) : super(key: key);
+  const AliceCallDetailsScreen({Key? key, required this.result}) : super(key: key);
 
   @override
   _AliceCallDetailsScreenState createState() => _AliceCallDetailsScreenState();
 }
 
-class _AliceCallDetailsScreenState extends alica_call_details_screen.State<AliceCallDetailsScreen>
-    with alica_call_details_screen.SingleTickerProviderStateMixin {
+class _AliceCallDetailsScreenState extends State<AliceCallDetailsScreen>
+    with SingleTickerProviderStateMixin {
 
   @override
   void initState() {
@@ -22,50 +22,50 @@ class _AliceCallDetailsScreenState extends alica_call_details_screen.State<Alice
   }
 
   @override
-  alica_call_details_screen.Widget build(alica_call_details_screen.BuildContext context) {
-    return alica_call_details_screen.Directionality(
-      textDirection: alica_call_details_screen.Directionality.of(context),
+  Widget build(BuildContext context) {
+    return Directionality(
+      textDirection: Directionality.of(context),
       child: _buildMainWidget(),
     );
   }
 
-  alica_call_details_screen.Widget _buildMainWidget() {
-    return alica_call_details_screen.DefaultTabController(
+  Widget _buildMainWidget() {
+    return DefaultTabController(
       length: 4,
-      child: alica_call_details_screen.Scaffold(
-        appBar: alica_call_details_screen.AppBar(
+      child: Scaffold(
+        appBar: AppBar(
           backgroundColor: Colors.blue,
-          leading: alica_call_details_screen.IconButton(
+          leading: IconButton(
             onPressed: (){
-              alica_call_details_screen.Navigator.of(context).pop();
+              Navigator.of(context).pop();
             },
-            icon: const alica_call_details_screen.Icon(alica_call_details_screen.Icons.arrow_back),
+            icon: const Icon(Icons.arrow_back),
           ),
-          bottom: alica_call_details_screen.TabBar(
-            indicatorColor: alica_call_details_screen.Colors.red,
+          bottom: TabBar(
+            indicatorColor: Colors.red,
             tabs: _getTabBars(),
           ),
-          title: const alica_call_details_screen.Text('Alice - HTTP Call Details'),
+          title: const Text('Alice - HTTP Call Details'),
         ),
-        body: alica_call_details_screen.TabBarView(
+        body: TabBarView(
           children: _getTabBarViewList(),
         ),
       ),
     );
   }
 
-  List<alica_call_details_screen.Widget> _getTabBars() {
-    final List<alica_call_details_screen.Widget> widgets = [];
-    widgets.add(const alica_call_details_screen.Tab(icon: alica_call_details_screen.Icon(alica_call_details_screen.Icons.info_outline), text: "Overview"));
-    widgets.add(const alica_call_details_screen.Tab(icon: alica_call_details_screen.Icon(alica_call_details_screen.Icons.arrow_upward), text: "Request"));
-    widgets.add(const alica_call_details_screen.Tab(icon: alica_call_details_screen.Icon(alica_call_details_screen.Icons.arrow_downward), text: "Response"));
-    widgets.add(const alica_call_details_screen.Tab(icon: alica_call_details_screen.Icon(alica_call_details_screen.Icons.warning), text: "Error",),
+  List<Widget> _getTabBars() {
+    final List<Widget> widgets = [];
+    widgets.add(const Tab(icon: Icon(Icons.info_outline), text: "Overview"));
+    widgets.add(const Tab(icon: Icon(Icons.arrow_upward), text: "Request"));
+    widgets.add(const Tab(icon: Icon(Icons.arrow_downward), text: "Response"));
+    widgets.add(const Tab(icon: Icon(Icons.warning), text: "Error",),
     );
     return widgets;
   }
 
-  List<alica_call_details_screen.Widget> _getTabBarViewList() {
-    final List<alica_call_details_screen.Widget> widgets = [];
+  List<Widget> _getTabBarViewList() {
+    final List<Widget> widgets = [];
     widgets.add(AliceCallOverviewWidget(result: widget.result));
     widgets.add(AliceCallRequestWidget(widget.result));
     widgets.add(AliceCallResponseWidget(widget.result));
