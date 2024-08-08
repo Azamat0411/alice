f5152624aad4fb0ebb88ad6b625d76a7ae4538f2
Azamat <69895564+Azamat0411@users.noreply.github.com>
2024-08-08 14:14:56 +0500

    Update alice_parser.dart
    alice_parser changes

--- lib/utils/alice_parser.dart
+++ lib/utils/alice_parser.dart
@@ -8,21 +8,9 @@ class AliceParser {
   static const String _parseFailedText = "Failed to parse ";
   static const JsonEncoder encoder = JsonEncoder.withIndent('  ');
 
-  // static String _parseJson(dynamic json) {
-  //   try {
-  //     return encoder.convert(json);
-  //   } catch (exception) {
-  //     return json.toString();
-  //   }
-  // }
-  //
-  // static dynamic _decodeJson(dynamic body) {
-  //   try {
-  //     return json.decode(body as String);
-  //   } catch (exception) {
-  //     return body;
-  //   }
-  // }
+  static String formatJson(Map<String, dynamic> json) {
+    return encoder.convert(json);
+  }
 
   static String formatBody(dynamic body, String? contentType) {
     try {
@@ -30,46 +18,8 @@ class AliceParser {
         return _emptyBody;
       }
 
-      var bodyContent = _emptyBody;
-      int a = 0, b = 0;
-      space(String body) {
-        String text = '';
-        for (int i = 0; i < body.toString().length; i++) {
-          if (body[i] == '[') {
-            a++;
-            text += '[\n' + '   ' * (a + b);
-          } else if (body[i] == '{') {
-            b++;
-            text += '{\n' + '   ' * (a + b);
-          } else if (body[i] == ',' && body[i + 1] == ' ') {
-            text += '${body[i]}\n' + '   ' * (a + b);
-            i++;
-          } else if (body[i] == ']') {
-            a--;
-            text += ']\n' + '   ' * (a + b);
-          }
-          else if (body[i] == '}') {
-            b--;
-            if (i < body.length - 1) {
-              if (body[i + 1] == ',' && i < body.length - 1) {
-                text += '\n' + '   ' * (a + b) + '},';
-                text += '\n' + '   ' * (a + b);
-                i++;
-              } else {
-                text += '}\n' + '   ' * (a + b);
-              }
-            } else {
-              text += '}\n' + '   ' * (a + b);
-            }
-          }
-          else {
-            text += body[i];
-          }
-        }
-        return text;
-      }
-      bodyContent = space(body);
-      return bodyContent;
+      String formattedJson = formatJson(body);
+      return formattedJson;
     } catch (exception) {
       return _parseFailedText + body.toString();
     }
